f61c8a2e797ad806c10305d33ac5c9dea233feba
Karolis Grinkus <karolis@kartais.lt>
2013-08-19 20:55:48 +0300

    Display posts in default view

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php get_header(); ?>
-<?php get_footer(); ?>
+<?php
+	get_header();
+
+	if(have_posts()) {
+		while(have_posts()) {
+			the_post();
+			the_content();
+		}
+	}
+
+	get_footer();
+?>
